50d439e202c8f95c6be729181145177770f3f50b
Mohammad <Mohammadd1098@gmail.com>
2019-01-18 13:40:06 +0300

    Adding the the toggle light bublb class and layout

---
+++ app/src/main/java/com/example/android/smarthome/RetrieveListOfDevicesController.java
@@ -0,0 +1,13 @@
+package com.example.android.smarthome;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RetrieveListOfDevicesController extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.retrieve_list_of_devices_boundary);
+    }
+}
