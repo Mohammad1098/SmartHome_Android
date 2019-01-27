7f096c8125f91a97d118c9cbca5b28264e45ac76
Rakans4 <ron4kan@gmail.com>
2019-01-27 22:59:19 +0300

    added basic UI

--- app/src/main/java/com/example/android/smarthome/Devices/RetrieveListOfDevicesController.java
+++ app/src/main/java/com/example/android/smarthome/Devices/RetrieveListOfDevicesController.java
@@ -3,6 +3,7 @@ package com.example.android.smarthome.Devices;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.android.smarthome.LightBulb.LightBulbBoundary;
 import com.example.android.smarthome.R;
@@ -14,7 +15,12 @@ public class RetrieveListOfDevicesController extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.retrieve_list_of_devices_boundary);
 
-        Intent intent = new Intent(this , LightBulbBoundary.class);
+//        Intent intent = new Intent(this , LightBulbBoundary.class);
+//        startActivity(intent);
+    }
+
+    public void lightActivity(View view){
+        Intent intent = new Intent(this, LightBulbBoundary.class);
         startActivity(intent);
     }
 }
